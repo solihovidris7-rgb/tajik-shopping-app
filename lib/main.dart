461c60ff2083bed47d1f361cdc91972f74548b8c
solihovidris7-rgb <solihovidris7@gmail.com>
2026-02-07 11:20:14 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Барномаи миёнарав',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Хуш омадед'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // баъдтар ин ҷо мегузарем ба саҳифаи фармоиш
+          },
+          child: Text('Фармоиш додан'),
+        ),
+      ),
+    );
+  }
+}
